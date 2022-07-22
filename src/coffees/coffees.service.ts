3e97476bf02fa91968b46759ecbd6771c79837b9
aldin.alagicc@gmail.com <aldin.alagicc@gmail.com>
2022-07-22 18:18:57 +0200

    Implement basic coffees service without db

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+    private coffees: Coffee[] = [
+        {
+            id: 1,
+            name: 'Shipwreck Roast',
+            brand: 'Buddy Brew',
+            flavors: ['chocolate', 'vanilla'],
+        },
+    ];
+
+    findall() {
+        return this.coffees;
+    }
+
+    findOne(id: string) {
+        return this.coffees.find(item => item.id === +id);
+    }
+
+    create(createCoffeeDto: any) {
+        this.coffees.push(createCoffeeDto);
+    }
+
+    remove(id: string) {
+        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
+        if (coffeeIndex >= 0) {
+            this.coffees.splice(coffeeIndex, 1);
+        }
+    }
+
+    update(id: string, updateCoffeeDto: any) {
+        const existingCoffee = this.findOne(id);
+        if (existingCoffee) {
+            for (const [key, value] of Object.entries(updateCoffeeDto)) {
+                if (key in existingCoffee) {
+                    existingCoffee[key] = value;
+                }
+            }
+
+            this.remove(id);
+            this.create(existingCoffee);
+        }
+    }
+
+}
