2094026bfd326a1a153d2b4270a08bc77fe335a3
aldin.alagicc@gmail.com <aldin.alagicc@gmail.com>
2022-07-22 18:49:39 +0200

    Add npx lint-staged

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,52 +1,56 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
-    private coffees: Coffee[] = [
-        {
-            id: 1,
-            name: 'Shipwreck Roast',
-            brand: 'Buddy Brew',
-            flavors: ['chocolate', 'vanilla'],
-        },
-    ];
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
 
-    findall() {
-        return this.coffees;
+  findall() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
     }
+    return coffee;
+  }
 
-    findOne(id: string) {
-        const coffee = this.coffees.find(item => item.id === +id);
-        if (!coffee) {
-            throw new NotFoundException(`Coffee #${id} not found`);
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      for (const [key, value] of Object.entries(updateCoffeeDto)) {
+        if (key in existingCoffee) {
+          existingCoffee[key] = value;
         }
-        return coffee;
+      }
+
+      this.remove(id);
+      this.create(existingCoffee);
     }
-
-    create(createCoffeeDto: any) {
-        this.coffees.push(createCoffeeDto);
-    }
-
-    remove(id: string) {
-        const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-        if (coffeeIndex >= 0) {
-            this.coffees.splice(coffeeIndex, 1);
-        }
-    }
-
-    update(id: string, updateCoffeeDto: any) {
-        const existingCoffee = this.findOne(id);
-        if (existingCoffee) {
-            for (const [key, value] of Object.entries(updateCoffeeDto)) {
-                if (key in existingCoffee) {
-                    existingCoffee[key] = value;
-                }
-            }
-
-            this.remove(id);
-            this.create(existingCoffee);
-        }
-    }
-
+  }
 }
