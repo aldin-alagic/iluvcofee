b435f995c6cd3af74c5b52a0c861381a620b8af6
aldin.alagicc@gmail.com <aldin.alagicc@gmail.com>
2022-07-25 00:01:13 +0200

    Clean up Coffees controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,34 +1,46 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
-    constructor(private readonly coffeesService: CoffeesService) {}
+  constructor(private readonly coffeesService: CoffeesService) {}
 
-    @Get()
-    findAll(@Query() paginationQuery) {
-        const { limit, offset } = paginationQuery;
-        return this.coffeesService.findall();
-    }
+  @Get()
+  findAll() {
+    return this.coffeesService.findall();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.coffeesService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.OK)
-    create(@Body() body) {
-        return this.coffeesService.create(body);
-    }
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  create(@Body() body: CreateCoffeeDto) {
+    return this.coffeesService.create(body);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() body) {
-        return this.coffeesService.update(id, body);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, body);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.coffeesService.remove(id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
 }
